ae21afda7afedd75143a566bcadaac5966648a72
Dustin_dusTir <du-wal@web.de>
2022-08-07 12:32:13 +0200

    testing routes and navigation

---
+++ app/src/routes/about.svelte
@@ -0,0 +1,6 @@
+<h1>This is about</h1>
+
+<ul>
+	<li><a href="/home">home</a></li>
+	<li><a href="/">index</a></li>
+</ul>
---
+++ app/src/routes/home.svelte
@@ -0,0 +1,6 @@
+<h1>This is home</h1>
+
+<ul>
+	<li><a href="/about">about</a></li>
+	<li><a href="/">index</a></li>
+</ul>
