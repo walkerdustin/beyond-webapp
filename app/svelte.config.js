4911a6b83c2bb77227b3485a96acb9606f37758a
Dustin_dusTir <du-wal@web.de>
2022-10-08 18:26:02 +0200

    migrate to latest sveltekit version

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -15,13 +15,8 @@ const config = {
 		  assets: 'build',
 		  fallback: null,
 		  precompress: false
-		}),
-	
-		prerender: {
-		  // This can be false if you're using a fallback (i.e. SPA mode)
-		  default: true
-		}
-	  },
+		})
+	  }
 };
 
 export default config;
