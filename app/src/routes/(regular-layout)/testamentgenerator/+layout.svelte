f2a142a1d44b1eec9899d13d6e86bb5bda960554
Dustin_dusTir <du-wal@web.de>
2022-11-20 15:32:34 +0100

    start on testament generator

---
+++ app/src/routes/(regular-layout)/testamentgenerator/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+    let generatorSections = ["Allgemeine Informationen", "Begünstigte", "Erbanteile", "Vermächtnisse", "Testamentverteilung", "Abschließende Klauseln"];
+    let numSections = generatorSections.length;
+</script>
+
+<div class="bg-gray-300 flex flex-grow h-96">
+    <div class="border-black border-solid m-32 mdc-elevation--z8 flex flex-grow  flex-col rounded-md bg-white">
+        <div class="m-4">
+            {#each generatorSections as section,i}
+                 <span class="ml-auto">section</span> 
+                 {#if i<numSections-1}
+                    <span class="ml-auto">></span> 
+                 {/if}
+            {/each}
+        </div>
+        <slot/>
+    </div>
+</div>
+
+<style>
+    @tailwind utilities;
+    @tailwind components;
+</style>
+
+
+
+
---
+++ app/src/routes/(regular-layout)/testamentgenerator/+page.svelte
@@ -0,0 +1,5 @@
+content
+
+<div class="flex flex-grow">
+
+</div>
