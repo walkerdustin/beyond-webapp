4ace59197abad71181cfe8a727a86e8d3388ab5f
Dustin_dusTir <du-wal@web.de>
2022-11-20 22:24:50 +0100

    im not sure if this does anything

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -2,6 +2,10 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		fontFamily: {
+			sans: ['Roboto', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		  },
 		extend: {}
 	},
 
