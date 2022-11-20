6c86d4c8b2facac67464a2238e1e6626fb52df60
Dustin_dusTir <du-wal@web.de>
2022-11-20 22:25:09 +0100

    layout for testamentgenerator

--- app/src/routes/(regular-layout)/testamentgenerator/+layout.svelte
+++ app/src/routes/(regular-layout)/testamentgenerator/+layout.svelte
@@ -1,27 +1,36 @@
 <script>
-    let generatorSections = ["Allgemeine Informationen", "Begünstigte", "Erbanteile", "Vermächtnisse", "Testamentverteilung", "Abschließende Klauseln"];
-    let numSections = generatorSections.length;
+	import LinearProgress from '@smui/linear-progress';
+
+	let progress = 0.1;
+
+	let generatorSections = [
+		'Allgemeine Informationen',
+		'Begünstigte',
+		'Erbanteile',
+		'Vermächtnisse',
+		'Testamentverteilung',
+		'Abschließende Klauseln'
+	];
+	let numSections = generatorSections.length;
+	let activeSection = 0;
 </script>
 
 <div class="bg-gray-300 flex flex-grow h-96">
-    <div class="border-black border-solid m-32 mdc-elevation--z8 flex flex-grow  flex-col rounded-md bg-white">
-        <div class="m-4">
-            {#each generatorSections as section,i}
-                 <span class="ml-auto">section</span> 
-                 {#if i<numSections-1}
-                    <span class="ml-auto">></span> 
-                 {/if}
-            {/each}
-        </div>
-        <slot/>
-    </div>
+	<div class="m-32 mdc-elevation--z8 flex flex-grow flex-col rounded-md bg-white">
+		<div class="m-4 flex flex-row">
+			{#each generatorSections as section, i}
+				<div class="mr-auto {activeSection === i ? 'font-bold' : ''}">{section}</div>
+				{#if i < numSections - 1}
+					<div class="mr-auto font-extrabold ">></div>
+				{/if}
+			{/each}
+		</div>
+		<LinearProgress {progress} {closed} />
+		<slot />
+	</div>
 </div>
 
 <style>
-    @tailwind utilities;
-    @tailwind components;
+	@tailwind utilities;
+	@tailwind components;
 </style>
-
-
-
-
