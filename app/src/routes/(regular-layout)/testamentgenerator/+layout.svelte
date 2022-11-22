fd20d86994731f18d81ed6f53b62a1ee243d7d73
Dustin_dusTir <du-wal@web.de>
2022-11-23 00:07:02 +0100

    upload changes

--- app/src/routes/(regular-layout)/testamentgenerator/+layout.svelte
+++ app/src/routes/(regular-layout)/testamentgenerator/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import LinearProgress from '@smui/linear-progress';
+	import IconButton from '@smui/icon-button';
+	import { mdiCommentQuestionOutline } from '@mdi/js';
+	import { Svg } from '@smui/common/elements';
 
 	let progress = 0.1;
 
@@ -15,7 +18,7 @@
 	let activeSection = 0;
 </script>
 
-<div class="bg-gray-300 flex flex-grow h-96">
+<div class="bg-gray-300 flex flex-grow">
 	<div class="m-32 mdc-elevation--z8 flex flex-grow flex-col rounded-md bg-white">
 		<div class="m-4 flex flex-row">
 			{#each generatorSections as section, i}
@@ -26,6 +29,15 @@
 			{/each}
 		</div>
 		<LinearProgress {progress} {closed} />
+		<div class="flex flex-row">
+			<span class="text-sm m-2">{progress * 100}%</span>
+			<div class="flex-grow" />
+
+			<IconButton component={Svg} viewBox="0 0 24 24" width="5px" height="5px"
+				><path fill="currentColor" d={mdiCommentQuestionOutline} />
+			</IconButton>
+		</div>
+
 		<slot />
 	</div>
 </div>
--- app/src/routes/(regular-layout)/testamentgenerator/+page.svelte
+++ app/src/routes/(regular-layout)/testamentgenerator/+page.svelte
@@ -1,5 +1,23 @@
-content
+<script>
+	import Card, { PrimaryAction } from '@smui/card';
 
-<div class="flex flex-grow">
+	let question = 'Was ist ihr Familienstand';
+	let option1 = 'Verheiratet';
+	let option2 = 'Nicht (mehr) verheiratet';
+</script>
 
+<div class="flex flex-grow justify-center flex-col">
+	<h3>{question}</h3>
+	<div class="flex flex-row">
+		<Card>
+			<PrimaryAction>
+				<p class="text-xl">{option1}</p>
+			</PrimaryAction>
+		</Card>
+		<Card>
+			<PrimaryAction>
+				<p class="text-xl">{option2}</p>
+			</PrimaryAction>
+		</Card>
+	</div>
 </div>
