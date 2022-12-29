e071ee49e32e0dbd5e9344b8274e5a8212fe2fad
Dustin_dusTir <du-wal@web.de>
2022-12-29 16:12:45 +0100

    set the daisy UI theme

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -19,6 +19,20 @@ const config = {
 	
 
 	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+		  {
+			mytheme: {
+			  primary: "#1B3F5F",
+			  secondary: "#285F8F",
+			  accent: "#37cdbe",
+			  neutral: "#3d4451",
+			  "base-100": "#ffffff",
+			},
+		  },
+		  "beyond",
+		],
+	  },
 };
 
 module.exports = config;
