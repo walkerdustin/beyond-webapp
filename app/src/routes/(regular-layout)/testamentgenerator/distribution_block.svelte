e1b8778f2bd87a41071944256e6928e148099497
Dustin_dusTir <du-wal@web.de>
2023-01-22 10:22:15 +0100

    header block mit verfügbare Quote soll hervorgehoben werden

--- app/src/routes/(regular-layout)/testamentgenerator/distribution_block.svelte
+++ app/src/routes/(regular-layout)/testamentgenerator/distribution_block.svelte
@@ -29,9 +29,11 @@
 <!--  a block, with the title, a textfield to display the current percentage 
 and a linear progress bar for visual -->
 <div
-	class="min-h-16 border-2 border-gray-50 rounded-md bg-surface grid grid-cols-3 mdc-elevation--z4 m-4 px-4 max-w-3xl"
+	class="{disabled
+		? 'h-20'
+		: 'min-h-16'}  border-2 border-gray-50 rounded-md bg-surface grid grid-cols-3 mdc-elevation--z4  m-4 px-4 max-w-3xl"
 >
-	<p class="my-auto">{title}</p>
+	<p class="my-auto  {disabled ? 'font-bold' : ''}">{title}</p>
 	<div class="my-auto">
 		<!-- <input bind:value={current_percentage} type="text" placeholder="Type here" class="input input-bordered input-primary w-xs" /> -->
 		{#if disabled}
@@ -58,15 +60,24 @@ and a linear progress bar for visual -->
 				<span class="text-xs font-bold">&nbsp; {gesetzliche_erbfolge} %</span>
 			</div>
 		{/if}
+		{#if disabled}
+			<LinearProgress
+				progress={current_percentage / 100}
+				buffer={current_percentage}
+				indeterminate={false}
+				{disabled}
+			/>
+		{:else}
+			<input
+				{disabled}
+				bind:value={current_percentage}
+				type="range"
+				min="0"
+				max="100"
+				class="range range-primary range-xs"
+			/>
+		{/if}
 
-		<input
-			{disabled}
-			bind:value={current_percentage}
-			type="range"
-			min="0"
-			max="100"
-			class="range range-primary range-xs"
-		/>
 		{#if pflichtanteil}
 			<div class="flex flex-col border-l border-black" style="margin-left: {pflichtanteil}%;">
 				<span class="text-xs font-bold">&nbsp; {pflichtanteil} %</span>
