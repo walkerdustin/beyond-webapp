0a8a67282f890fc343e7503607e475c99b6317c4
Dustin_dusTir <du-wal@web.de>
2023-03-18 13:39:23 +0100

    Add a direct link to the tesament generator

--- app/src/routes/(regular-layout)/+page.svelte
+++ app/src/routes/(regular-layout)/+page.svelte
@@ -1,4 +1,18 @@
 <script>
+	import Button, { Label } from '@smui/button';
+	import Ripple from '@smui/ripple';
 </script>
 
-<h1>Index</h1>
+<div class="flex items-center justify-center center flex-grow">
+	<!-- <Button color="secondary" variant="raised">
+		<a href="/testamentgenerator"> Testamentgenerator <br /> starten</a>
+	</Button> -->
+
+	<div
+		class="flex p-2 rounded items-center btn-primary text-white font-medium text-center mt-20"
+		use:Ripple={{ surface: true, color: 'primary' }}
+	>
+		<a href="/testamentgenerator">Testamentgenerator <br /> starten</a>
+	</div>
+</div>
+
